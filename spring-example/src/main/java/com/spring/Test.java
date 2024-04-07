94c2bb45d0d764ac48cf3e45849313ae542b9150
programmer_cj <programmer_cj@163.com>
2024-04-07 21:13:58 +0800

    1

--- spring-example/src/main/java/com/spring/Test.java
+++ spring-example/src/main/java/com/spring/Test.java
@@ -1,8 +1,10 @@
 package com.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Test {
 
     public static void main(String[] args) {
-
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
     }
 }
